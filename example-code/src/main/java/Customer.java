ce59ec020fe20f11b22445ef1a876199a90f50b8
이정호 <dlwjdgh1115@gmail.com>
2024-06-27 18:39:51 +0900

    setting

---
+++ example-code/src/main/java/Customer.java
@@ -0,0 +1,47 @@
+import java.util.Enumeration;
+import java.util.Vector;
+
+public class Customer {
+    private String _name;
+    private Vector _rentals = new Vector();
+
+    public Customer(String name) {
+        _name = name;
+    }
+
+    public void addRental(Rental arg) {
+        _rentals.addElement(arg);
+    }
+
+    public String getName() {
+        return _name;
+    }
+
+    public String statement() {
+        double totalAmount = 0;
+        int frequentRenterPoints = 0;
+        Enumeration rentals = this._rentals.elements();
+        String result = "Rental Record for " + this.getName() + "\n";
+
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+
+            // 각 영화에 대한 요금 결정
+            double thisAmount = each.getFeeForEachMovie();
+
+            // 포인트 추가
+            frequentRenterPoints = each.getRenterPoint();
+
+            // 이 대여에 대한 요금 계산 결과 표시
+            result += "\t" + each.getMovie().getTitle() + "\t" +
+                    String.valueOf(thisAmount) + "\n";
+            totalAmount += thisAmount;
+        }
+
+        // 풋터(footer) 추가
+        result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
+        result += "You earned " + String.valueOf(frequentRenterPoints) +
+                "frequent renter points";
+        return result;
+    }
+}
---
+++ example-code/src/main/java/Rental.java
@@ -0,0 +1,52 @@
+public class Rental {
+    private Movie _movie;
+    private int _daysRented;
+
+    public Rental(Movie movie, int daysRented) {
+        _movie = movie;
+        _daysRented = daysRented;
+    }
+
+    public int getDaysRented() {
+        return _daysRented;
+    }
+
+    public Movie getMovie() {
+        return _movie;
+    }
+
+    public double getFeeForEachMovie() {
+        double result = 0;
+        switch (this._movie.getPriceCode()) {
+            case Movie.REGULAR:
+                result += 2;
+                if (this.getDaysRented() > 2) {
+                    result += (this.getDaysRented() - 2) * 1.5;
+                }
+                break;
+            case Movie.NEW_RELEASE:
+                result += this.getDaysRented() * 3;
+                break;
+            case Movie.CHILDREN:
+                result += 1.5;
+                if (this.getDaysRented() > 3) {
+                    result += (this.getDaysRented() - 3) * 1.5;
+                }
+                break;
+        }
+        return result;
+    }
+
+    public int getRenterPoint() {
+        int renterPoint = 1;
+
+        // 최신을 이틀 이상 대여하는 경우 추가 포인트 제공
+        boolean isNewReleaseMovie = this._movie.getPriceCode() == Movie.NEW_RELEASE;
+        boolean isRentedDayAfterDay = this.getDaysRented() > 1;
+        if (isNewReleaseMovie && isRentedDayAfterDay) {
+            return renterPoint + 1;
+        }
+
+        return renterPoint;
+    }
+}
